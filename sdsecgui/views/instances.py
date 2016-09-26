cac93dd568832975349455513dbd75a72d6184ef
jinsikKang <kangjinsik@gmail.com>
2016-09-26 10:05:08 +0900

    test

--- sdsecgui/views/instances.py
+++ sdsecgui/views/instances.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 from sdsec import log_handler
 from sdsec.log_handler import setLogDir, getLogger
-from sdsecgui.tools.command import getInstanceList
+from sdsecgui.tools.command import getInstanceList, login
 from sdsecgui.cmodels.instance import Instance
 
 setLogDir()
@@ -13,6 +13,7 @@ logger = getLogger()
 
 def retrieveInstanceList(request):
     logger.info("retrieveInstanceList")
+    login("admin", "chiron", "admin", "192.168.10.6")
     instanceList = getInstanceList()
     return render(request, 'instances/index.html', { 'instanceList' : instanceList })
 
