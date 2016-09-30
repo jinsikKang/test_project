33393ecdb37a5988b1ae5314017998580a28863c
jinsikKang <kangjinsik@gmail.com>
2016-09-30 18:16:26 +0900

    test

--- sdsecgui/dashboard/admin/instances/views.py
+++ sdsecgui/dashboard/admin/instances/views.py
@@ -14,10 +14,10 @@ def retrieveInstanceList(request):
     logger.info("retrieveInstanceList")
     login("admin", "chiron", "admin", "192.168.10.6", ":35357/v2.0")
     instanceList = getInstanceList()
-    return render(request, 'dashboard/admin/instances/templates/index.html', { 'instanceList' : instanceList })
+    return render(request, 'index.html', { 'instanceList' : instanceList })
 
 def retrieveInstanceById(request, instance_id):
     logger.info("retrieveInstanceById")
     instance = Instance()
     instance.setById(instance_id)
-    return render(request, 'admin/instances/info.html', { 'instance' : instance })
+    return render(request, 'info.html', { 'instance' : instance })
