e242c36aab324c33fcc5c97a034f271347462b64
jinsikKang <kangjinsik@gmail.com>
2016-10-01 22:54:13 +0900

    test

--- sdsecgui/dashboard/admin/networks/urls.py
+++ sdsecgui/dashboard/admin/networks/urls.py
@@ -5,6 +5,6 @@ from sdsecgui.dashboard.admin.networks import views
 
 urlpatterns = [
     url(r'^$', views.retrieveNetworkList, name='networkList'),
-    url(r'(?P<network_id>[\w\-]+)/$', views.retrieveNetworkById, name='network'),
-    url(r'subnets/(?P<subnet_id>[\w\-]+)/$', views.retrieveSubnetById, name='subnet'),
+    url(r'^(?P<network_id>[\w\-]+)/$', views.retrieveNetworkById, name='network'),
+    url(r'^subnets/(?P<subnet_id>[\w\-]+)/$', views.retrieveSubnetById, name='subnet'),
 ]
