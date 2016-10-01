38d38e45e7f448e6b7d4152cadde76373c39598d
jinsikKang <kangjinsik@gmail.com>
2016-10-01 19:14:01 +0900

    test

--- sdsecgui/cmodels/network.py
+++ sdsecgui/cmodels/network.py
@@ -1,5 +1,6 @@
 # _*_coding:utf-8_*_
 import json
+import re
 
 from sdsec.log_handler import setLogDir, getLogger
 from ..tools.command import excuteCmd, login
@@ -144,3 +145,15 @@ class Subnet(Base):
         cls.id = subnetDic["id"]
         cls.subnetpool_id = subnetDic["subnetpool_id"]
         cls.name = subnetDic["name"]
+        ipv4_pattern = re.compile("[\d]+\.[\d]+\.[\d]+\.([\d]+)")
+        ipv6_pattern = re.compile("[\w]+:[\w]+:[\w]+:[\w]*:([\w:]+)")
+        """
+        ^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$
+        """
+        if cls.ip_version == "4":
+            print ipv4_pattern.match(cls.allocation_pools["end"]).group(1)
+            print ipv4_pattern.match(cls.allocation_pools["start"]).group(1)
+        elif cls.ip_version == "6":
+            # ipv6_pattern
+            pass
+        cls.remain_ip = cls.allocation_pools["start"]
