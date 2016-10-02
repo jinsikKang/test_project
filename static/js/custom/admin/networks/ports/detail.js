4dbab50e8f09d14c60e44088430c2e4914fd86d5
jinsikKang <kangjinsik@gmail.com>
2016-10-03 02:51:14 +0900

    test

--- static/js/custom/admin/networks/ports/detail.js
+++ static/js/custom/admin/networks/ports/detail.js
@@ -7,13 +7,13 @@ function getPostAjax(id, csrf_token){
             $(".header_title_d01").html(data.port.name); // header 셋팅
             for( key in data.port ){    // 데이터 넣기
                 var resultHtml = "";
-                var tempList = data.port.fixed_ip.split("\n");
+                var tempList = data.port.fixed_ips.split("\n");
                 var fixed_ips = [];
                 for( i in tempList ){
                     var fixed_ip = JSON.parse(tempList[i]);
                     fixed_ips.push(fixed_ip);
                 }
-                if (key == "fixed_ip") {
+                if (key == "fixed_ips") {
                     for( var i = 0; i < fixed_ips.length; i++ ){
                         if( resultHtml != "" ){
                             resultHtml += "<br/>";
