058325529dfc69b86b6b59c12d09f9506da5b152
jinsikKang <kangjinsik@gmail.com>
2016-10-03 02:49:55 +0900

    test

--- static/js/custom/admin/networks/ports/detail.js
+++ static/js/custom/admin/networks/ports/detail.js
@@ -7,7 +7,22 @@ function getPostAjax(id, csrf_token){
             $(".header_title_d01").html(data.port.name); // header 셋팅
             for( key in data.port ){    // 데이터 넣기
                 var resultHtml = "";
-                resultHtml = data.port[key];
+                var tempList = data.port.fixed_ip.split("\n");
+                var fixed_ips = [];
+                for( i in tempList ){
+                    var fixed_ip = JSON.parse(tempList[i]);
+                    fixed_ips.push(fixed_ip);
+                }
+                if (key == "fixed_ip") {
+                    for( var i = 0; i < fixed_ips.length; i++ ){
+                        if( resultHtml != "" ){
+                            resultHtml += "<br/>";
+                        }
+                        resultHtml += "IP주소 " + fixed_ips[i]["ip_address"] + " 서브넷 ID " + fixed_ips[i]["subnet_id"];
+                    }
+                } else {
+                    resultHtml = data.port[key];
+                }
                 if( resultHtml == "" ){
                     resultHtml = "None"
                 }
