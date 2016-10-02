108de7756c8d98928591eac5f6a40a395675f418
jinsikKang <kangjinsik@gmail.com>
2016-10-03 00:20:21 +0900

    test

--- static/js/custom/data_table.js
+++ static/js/custom/data_table.js
@@ -11,40 +11,50 @@ function DataTable (settings){
     }else{
         this.data = null;
     }
+    this.link = "";
+
     this.showDataTable = function() {
-        var columnPattern = "<th class='ind_th01 %K'>%C</th>\n"
-        var dataPattern = "<td class='ind_td01 %K'>%D</td>\n";
+        /* 컬럼과 데이터 형식 */
+        var columnForm = "<th class='ind_th01 %K'>%C</th>\n"
+        var dataForm = "<td class='ind_td01 %K'>%D</td>\n";
         var resultHtml = ""
-        if( this.vertical ){
+
+        if( this.vertical ){ /* 데이터를 세로로 출력*/
             for( key in this.columns ){
                 resultHtml += "<tr>\n";
                 var columnHtml = "";
                 var dataHtml = "";
+
+                if ( this.columns[key].indexOf(":link") != -1 ) {
+                    /* class 에 link 를 포함 */
+                    this.columns[key] = this.columns[key].replace(":link", "");
+                    this.link = " link";
+                }
                 if ( this.columns[key] instanceof Object ) {
                     // 객체일때 여러줄로 출력
-                    columnHtml = columnPattern.replace("%C", this.columns[key].name).replace("%K", key);
+                    columnHtml = columnForm.replace("%C", this.columns[key].name).replace("%K", key);
                     var data = "";
                     for ( dataNameKey in this.columns[key].dataName ){
                         data += this.columns[key].dataName[dataNameKey] + " " + this.data[key][dataNameKey] + "<br/>";
                     }
-                    dataHtml = dataPattern.replace("%D", data).replace("%K", key);
+                    dataHtml = dataForm.replace("%D", data).replace("%K", key + link);
                 } else if ( this.data[key] instanceof Array ) {
                     // 배열일때 ul, li태그로 묶어 출력
-                    columnHtml = columnPattern.replace("%C", this.columns[key]).replace("%K", key);
+                    columnHtml = columnForm.replace("%C", this.columns[key]).replace("%K", key);
                     var data = "<ul>";
                     for ( i in this.data[key]){
                         data += "<li>" + this.data[key][i] + "</li>";
                     }
                     data += "</ul>";
-                    dataHtml = dataPattern.replace("%D", data).replace("%K", key);
+                    dataHtml = dataForm.replace("%D", data).replace("%K", key + link);
                 } else {
-                    columnHtml = columnPattern.replace("%C", this.columns[key]).replace("%K", key);
+                    columnHtml = columnForm.replace("%C", this.columns[key]).replace("%K", key);
                     if( this.data[key] instanceof Object ){
                         for ( dataNameKey in this.data[key] ){
-                            dataHtml = dataPattern.replace("%D", this.data[key][dataNameKey]).replace("%K", key);
+                            dataHtml = dataForm.replace("%D", this.data[key][dataNameKey]).replace("%K", key + link);
                         }
                     } else {
-                        dataHtml = dataPattern.replace("%D", this.data[key]).replace("%K", key);
+                        dataHtml = dataForm.replace("%D", this.data[key]).replace("%K", key + link);
                     }
                 }
                 var data = this.data[key];
@@ -63,7 +73,7 @@ function DataTable (settings){
             for( i in this.data ){
                 dataHtml += "<tr>\n";
                 for( key in this.columns ){
-                    dataHtml += "<td class='ind_td01 " + key + "'>" + this.data[i][key] + "</td>\n";
+                    dataHtml += "<td class='ind_td01 " + key + link + "'>" + this.data[i][key] + "</td>\n";
                 }
                 dataHtml += "</tr>\n";
             }
@@ -71,5 +81,17 @@ function DataTable (settings){
         }
         $(this.selector + " table").html(resultHtml);
     };
+
+    this.setLink = function(url) {
+        var linkTagList = $(this.selector + " table .link").get();
+        for ( i in linkTagList ){
+            var linkTag = linkTagList.get(i);
+            var beforHtml = linkTag.html();
+            var resultHtml = "<a href='/dashboard/admin/" + url + "/" + this.data[i].id + "'>" + beforHtml + "</a>";
+            linkTag.html(resultHtml);
+        }
+
+    }
+
     return this;
 }
