0983d93c7f678013aa950fb8a7b2c8d5a3d8bcbd
jinsikKang <kangjinsik@gmail.com>
2016-10-03 03:27:00 +0900

    test

--- static/js/custom/admin/networks/ports/detail.js
+++ static/js/custom/admin/networks/ports/detail.js
@@ -26,7 +26,7 @@ function getPostAjax(id, csrf_token){
                     }
                 } else if (key.indexOf("binding") != -1){
                     if (key == "binding:vif_details") {
-                        var vif_details = getListData(data.port.vif_details);
+                        var vif_details = getListData(data.port["binding:vif_details"]);
                         resultHtml += "<ul>"
                         for( var i = 0; i < vif_details.length; i++ ){
                             resultHtml += "<li><b>port_filter</b>" + vif_details[i]["port_filter"] + "</li><li><b>ovs_hybrid_plug</b>" + vif_details[i]["ovs_hybrid_plug"] + "</li>";
