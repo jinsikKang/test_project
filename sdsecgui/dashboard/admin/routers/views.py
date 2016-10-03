58c3cca970a2d35b97daf090060a2ac3842cc869
jinsikKang <kangjinsik@gmail.com>
2016-10-03 13:58:43 +0900

    test

--- sdsecgui/dashboard/admin/routers/views.py
+++ sdsecgui/dashboard/admin/routers/views.py
@@ -12,19 +12,16 @@ from sdsecgui.cmodels.router import Router
 
 def retrieveRouterList(request):
     # logger.info("retrieveRouterList")
-    if request.is_ajax() and request.method == 'POST':
-        tempList = []
-        pprint.pprint(request)
-        for router in request.routerList:
-            router_id = router["id"]
-            router = Router()
-            router.setById(router_id)
-            tempList.append(router.showInfoJsonById(router_id))
-        routerList = tempList
-        return JsonResponse({'data': routerList})
-    else:
-        routerList = getRouterList().replace('"','\\"').replace("'","\\'")
-        return render(request, 'admin/routers/index.html', { 'routerList' : routerList })
+    routerList = getRouterList()
+    tempList = []
+    pprint.pprint(request)
+    for router in request.routerList:
+        router_id = router["id"]
+        router = Router()
+        router.setById(router_id)
+        tempList.append(router.showInfoJsonById(router_id))
+    routerList = tempList
+    return render(request, 'admin/routers/index.html', { 'routerList' : routerList })
 
 def retrieveRouterById(request, router_id):
     if request.is_ajax() and request.method == 'POST':
