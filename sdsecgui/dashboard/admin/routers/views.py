9d400a00127c9e28954eede4a11856cdd4496ebf
jinsikKang <kangjinsik@gmail.com>
2016-10-03 15:23:40 +0900

    test

--- sdsecgui/dashboard/admin/routers/views.py
+++ sdsecgui/dashboard/admin/routers/views.py
@@ -14,6 +14,7 @@ def retrieveRouterList(request):
     # logger.info("retrieveRouterList")
     if request.is_ajax() and request.method == 'POST':
         tempList = []
+        print request.POST
         data = json.loads(request.POST.routerList)
         print data
         for router in request.body:
