935de7c0c8070b6d7b89d4bccc98f41fe9925248
jinsikKang <kangjinsik@gmail.com>
2016-10-08 19:07:53 +0900

    test

--- static/js/custom/admin/networks/info.js
+++ static/js/custom/admin/networks/info.js
@@ -96,22 +96,6 @@ $(function(){
     $("#subnet").hide();
     $("#port").hide();
     $("#DHCPagent").hide();
-    function tabClick(i, tempTabList){
-        var tabList = [];
-        for (idx in tempTabList) {
-            var tempTab = tempTabList[idx];
-            tabList.push(tempTab);
-        }
-        var tab = tabList.splice(i, 1);
-        for (j in tabList) {
-            $("#" + tabList[j]).hide();
-            $("." + tabList[j]).removeClass("header_title_d04_click");
-            $("." + tabList[j]).addClass("header_title_d05");
-        }
-        $("." + tab).addClass("header_title_d04_click");
-        $("." + tab).removeClass("header_title_d05");
-        $("#" + tab).show();
-    }
     var tabList = ["summary", "subnet", "port", "DHCPagent"];
     $(".summary").on("click", function(){tabClick(0, tabList)});
     $(".subnet").on("click", function(){tabClick(1, tabList)});
