dc15afedce3a703742495fae5b253deed321042b
jinsikKang <kangjinsik@gmail.com>
2016-10-13 16:30:34 +0900

    test

--- sdsecgui/dashboard/admin/instances/views.py
+++ sdsecgui/dashboard/admin/instances/views.py
@@ -1,9 +1,6 @@
 #_*_coding:utf-8_*_
 
 from django.shortcuts import render
-from django.core.urlresolvers import reverse
-
-from horizon import exceptions
 
 from sdsec.log_handler import setLogDir, getLogger
 from sdsecgui.tools.command import getInstanceList, login
