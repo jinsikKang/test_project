3eca41d010eb530a54173d9bb4f0b768a1bf827f
jinsikKang <kangjinsik@gmail.com>
2016-10-17 11:47:18 +0900

    test

--- static/js/custom/data_table.js
+++ static/js/custom/data_table.js
@@ -87,9 +87,11 @@ function DataTable (settings){
             dataHtml += "<tr>\n";
             for( key in this.columns ){
                 var link = "";
-                if ( typeof this.columns[key] === "string" && this.columns[key].indexOf(":link") != -1 ) {
-                    /* class 에 link 를 포함 */
-                    link = " link";
+                if (typeof this.columns[key] === "string"){
+                    if ( this.columns[key].indexOf(":link") != -1 ) {
+                        /* class 에 link 를 포함 */
+                        link = " link";
+                    }
                 }
                 if ( key.indexOf(".") != -1 ){
                     var mainKey = key.replace(/\.\w+/g, "");
