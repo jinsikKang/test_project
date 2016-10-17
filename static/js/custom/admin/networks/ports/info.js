40bff01ff87e9b590df5569ea845db8521c4915f
jinsikKang <kangjinsik@gmail.com>
2016-10-17 11:28:31 +0900

    test

--- static/js/custom/admin/networks/ports/info.js
+++ static/js/custom/admin/networks/ports/info.js
@@ -1,13 +1,4 @@
 function getPortAjax(id, csrf_token){
-    this.getListData = function(dataList){
-        var tempList = dataList.split("\n");
-        var resultList = [];
-        for( i in tempList ){
-            var data = JSON.parse(tempList[i]);
-            resultList.push(data);
-        }
-        return resultList
-    }
     $.ajax({
         type:"POST",
         url : '',
@@ -17,7 +8,7 @@ function getPortAjax(id, csrf_token){
             for( key in data.port ){    // 데이터 넣기
                 var resultHtml = "";
                 if (key == "fixed_ips") {
-                    var fixed_ips = getListData(data.port.fixed_ips);
+                    var fixed_ips = data.port.fixed_ips;
                     for( var i = 0; i < fixed_ips.length; i++ ){
                         if( resultHtml != "" ){
                             resultHtml += "<br/>";
@@ -26,7 +17,7 @@ function getPortAjax(id, csrf_token){
                     }
                 } else if (key.indexOf("binding") != -1){
                     if (key == "binding:vif_details") {
-                        var vif_details = getListData(data.port["binding:vif_details"]);
+                        var vif_details = data.port["binding:vif_details"];
                         resultHtml += "<ul>"
                         for( var i = 0; i < vif_details.length; i++ ){
                             resultHtml += "<li><b>port_filter</b> " + vif_details[i]["port_filter"] + "</li><li><b>ovs_hybrid_plug</b> " + vif_details[i]["ovs_hybrid_plug"] + "</li>";
